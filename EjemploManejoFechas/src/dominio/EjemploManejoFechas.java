df6db7a66e90d2072fe1b0bd375683b9beaae39b
Noelia Bentancor <noeliabentancor1@gmail.com>
2022-10-23 23:07:26 -0300

    Adds exercise 1,2,3,4 and sample for handling dates

---
+++ EjemploManejoFechas/src/dominio/EjemploManejoFechas.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dominio;
+
+import interfaz.VentanaMenu;
+
+/**
+ *
+ * @author PC
+ */
+public class EjemploManejoFechas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaMenu vent= new VentanaMenu();
+        vent.setVisible(true);
+    }
+    
+}
